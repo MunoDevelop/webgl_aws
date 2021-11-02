e51957ad882c90b99f2cc4c1983285c689384251
DESKTOP-VDFLBFT\user <huozuyinshua@gmail.com>
2021-11-03 00:05:41 +0900

    day2

--- src/DevName.js
+++ src/DevName.js
@@ -4,6 +4,7 @@ import {FontLoader} from "three/examples/jsm/loaders/FontLoader";
 import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry";
 import './DevName.css';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {Reflector} from "three/examples/jsm/objects/Reflector";
 
 class DevName extends React.Component {
     constructor(props) {
@@ -19,46 +20,73 @@ class DevName extends React.Component {
         const height = (window.innerHeight )/4;
 
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(100, width / height, 0.4, 1500);
-        camera.position.set( 0, 30, 80 );
+        const camera = new THREE.PerspectiveCamera(65, 2, 0.4, 1500);
+        camera.position.set( 0, 30, 170 );
         //let cameraTarget = new THREE.Vector3( 0, 0, 0 );
         //camera.lookAt(cameraTarget);
-        const renderer = new THREE.WebGLRenderer();
+        const renderer = new THREE.WebGLRenderer({antialias: true});
         renderer.setSize(width, height);
         //background
         scene.background = new THREE.CubeTextureLoader()
             .setPath( '' )
             .load( [
-                'posx.jpg',
-                'negx.jpg',
-                'posy.jpg',
-                'negy.jpg',
-                'posz.jpg',
-                'negz.jpg'
+                'posX.png',
+                'negX.png',
+                'posY.png',
+                'negY.png',
+                'posZ.png',
+                'negZ.png'
             ] )
 
         // LIGHTS
-        const dirLight = new THREE.DirectionalLight( 0xffffff, 0.625 );
-        dirLight.position.set( 0, 0, 1 ).normalize();
-        scene.add( dirLight );
+        // const dirLight = new THREE.DirectionalLight( 0xffffff, 0 );
+        // dirLight.position.set( 0, 0, 1 ).normalize();
+        // scene.add( dirLight );
+
+        const tinkerbellGeo = new THREE.SphereGeometry(2,32,16);
+
+        const tinkerbellMat = new THREE.MeshBasicMaterial({});
+
+        const tinkerbell = new THREE.PointLight( 0xffee88, 4,100);
+        const tinkerbellMesh = new THREE.Mesh( tinkerbellGeo, tinkerbellMat );
+
+        tinkerbell.add(tinkerbellMesh);
+        tinkerbell.castShadow = true;
+        //tinkerbell.castShadow = true;
+        scene.add( tinkerbell );
+        //tinkerbell.target.position.set(0, 0, 0);
+        //scene.add(tinkerbell.target);
+        //tinkerbell.position.set(0,50,100);
+        //const helper = new THREE.DirectionalLightHelper(tinkerbell);
+        //scene.add(helper);
+        //tinkerbell.target.updateMatrixWorld();
+        //helper.update();
+        this.tinkerbell = tinkerbell;
+        this.tinkebellPosX = 0;
+        this.tinkebellPosZ = 0;
+        this.tinkebellAng = 0;
 
         // const pointLight = new THREE.PointLight( 0xffffff, 0.2 );
         // pointLight.position.set( 0, 0, 0 );
         // scene.add( pointLight );
 
-        // FONT
+        // TEXT PART
         const loader = new FontLoader();
         var font;
         let text = "노문호";
+        const baseColor = new THREE.Color("hsl(181,82%,47%)");
+        const wrampTexture = new THREE.TextureLoader().load('fiveTone.jpg');
+        // const TileBaseTexture = new THREE.TextureLoader().load('TileTex.jpg');
+        // const TileNormalTexture = new THREE.TextureLoader().load('TileTex_Normal.jpg');
+        wrampTexture.minFilter = THREE.NearestFilter;
+        wrampTexture.magFilter = THREE.NearestFilter;
+        //wrampTexture.minFilter = THREE.NearestFilter;
+        //wrampTexture.magFilter = THREE.NearestFilter;
+        let textMaterial = new THREE.MeshToonMaterial({color:baseColor,gradientMap:wrampTexture});
         //console.log('Current directory: ' + window.location.href);
         loader.load('Do Hyeon_Regular.json',function(font){
-            const baseColor = new THREE.Color('#2fb7e0');
-            let textMaterial = new THREE.MeshToonMaterial({color:baseColor});
-            //THREE.MeshBasicMaterial({color:0x035b59});
             let textGeo = new TextGeometry( text, {
-
                 font: font,
-
                 size: 50,
                 height: 20,
                 curveSegments: 4,
@@ -86,6 +114,17 @@ class DevName extends React.Component {
 
         },null,null);
 
+        // PLANE PART
+
+        const plane_geometry = new THREE.PlaneGeometry( 300, 200 );
+        //const plane_material = new THREE.MeshBasicMaterial( {color: 0xffffff, opacity:0.5,transparent:true} );
+        //const plane = new THREE.Mesh( plane_geometry, plane_material );
+        const vertialMirror = new Reflector(plane_geometry,{clipBias:0.003,color:new THREE.Color(0xFFFFFF)});
+        vertialMirror.position.set(0,-5,0);
+        vertialMirror.rotateX(-1.5708);
+        scene.add( vertialMirror );
+
+
 
 
         this.element.appendChild(renderer.domElement);
@@ -109,7 +148,18 @@ class DevName extends React.Component {
         //let tempMesh = this.textMesh1?;
         //this.textMesh1.rotation.set(0,0,0,'xyz')
         requestAnimationFrame(this.animate);
+
+
         this.controls.update();
+        this.tinkebellAng -= 0.05;
+        this.tinkebellAng %=360;
+        this.tinkebellPosX = 200 * Math.cos(this.tinkebellAng);
+        this.tinkebellPosZ = 80 * Math.sin(this.tinkebellAng);
+        this.tinkerbell.position.set(this.tinkebellPosX,40,this.tinkebellPosZ);
+
+
+
+
         this.renderer.render(this.scene, this.camera);
     }
 
--- src/DrawBlock.js
+++ src/DrawBlock.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as THREE from 'three';
 import {DataTextureLoader} from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 class DrawBlock extends React.Component {
     constructor(props) {
@@ -12,7 +13,7 @@ class DrawBlock extends React.Component {
         const height = (window.innerHeight - 1)/3;
 
         var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
+        var camera = new THREE.PerspectiveCamera(50, 2, 1, 1000);
 
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(width, height);
@@ -23,21 +24,35 @@ class DrawBlock extends React.Component {
 
         this.element.appendChild(renderer.domElement);
 
-        //scene env part
+        //scene sky sphere part
+        // let skyGeo = new THREE.SphereGeometry(65, 25, 25);
+        // let loader  = new THREE.TextureLoader();
+        // let texture = loader.load( "skDom.png" );
+        // let skyMat = new THREE.MeshPhongMaterial({
+        //     map: texture,
+        // });
+        // var sky = new THREE.Mesh(skyGeo, skyMat);
+        //sky.material.side = THREE.BackSide;
+        //scene.add(sky);
+
         scene.background = new THREE.CubeTextureLoader()
             .setPath( '' )
             .load( [
-                'posx.jpg',
-                'negx.jpg',
-                'posy.jpg',
-                'negy.jpg',
-                'posz.jpg',
-                'negz.jpg'
+                'posX.png',
+                'negX.png',
+                'posY.png',
+                'negY.png',
+                'posZ.png',
+                'negZ.png'
+
             ] )
-        const wrampTexture = new THREE.TextureLoader().load('toonramp.jpg');
+
+
+
+        const wrampTexture = new THREE.TextureLoader().load('fiveTone.jpg');
         const geometry = new THREE.SphereGeometry(2,32,16);
 
-        const baseColor = new THREE.Color('#2fb7e0');
+        const baseColor = new THREE.Color("rgb(25, 165, 230)");
         let material = new THREE.MeshToonMaterial({color:baseColor,gradientMap:wrampTexture});
         //const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         const cube = new THREE.Mesh(geometry, material);
@@ -46,8 +61,11 @@ class DrawBlock extends React.Component {
         const dirLight = new THREE.DirectionalLight( 0xffffff, 0.725 );
         dirLight.position.set( 0, 0, 1 ).normalize();
         this.scene.add( dirLight );
-        this.camera.position.z = 5;
 
+
+        this.camera.position.z = 5;
+        const controls = new OrbitControls(camera, renderer.domElement );
+        this.controls = controls;
 
         this.cube = cube;
         this.animate();
@@ -55,10 +73,12 @@ class DrawBlock extends React.Component {
 
 
     animate = () => {
-        this.renderer.render(this.scene, this.camera);
+
         this.cube.rotation.x += 0.01;
         this.cube.rotation.y += 0.01;
         requestAnimationFrame(this.animate);
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
     }
 
     render() {
